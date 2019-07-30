c45e9ba3c0acdbe884d345de3f826cca7e21dca0
Jehiel Martinez <jehielmartinez@gmail.com>
2019-07-30 03:31:12 -0600

    new-duty route added

--- routes/dutiesRoutes.js
+++ routes/dutiesRoutes.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const Duty = require('../models/dutyModel')
 
+//NEW DUTY
+router.post('/new-duty', async (req, res) => {
 
+    const duty = new Duty(req.body)
+
+    try {
+        await duty.save()
+        res.status(201).send(duty)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
 
 module.exports = router
