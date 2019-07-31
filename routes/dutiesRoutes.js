25f0b3d53f742693d5a76a181216c8956af21901
Jehiel Martinez <jehielmartinez@gmail.com>
2019-07-30 23:56:42 -0600

    get-duty and edit-duty routes added

--- routes/dutiesRoutes.js
+++ routes/dutiesRoutes.js
@@ -16,4 +16,22 @@ router.post('/new-duty', async (req, res) => {
     }
 })
 
+router.get('/get-duty', async(req, res) => {
+    try {
+        const duty = await Duty.findById(req.body.id)
+        res.status(200).send(duty)
+    } catch (error) {
+        res.status(404).send(error)
+    }
+})
+
+router.patch('/edit-duty', async (req, res) => {
+    try {
+        const duty = Duty.findByIdAndUpdate(req.body._id, req.body)
+        res.status(200).send(duty)
+    } catch (error) {
+        res.status(404).send(error)
+    }
+})
+
 module.exports = router
