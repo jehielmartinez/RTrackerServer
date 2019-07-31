912f724e8f3ebb1a5a2f86f7fe9471d497750fd6
Jehiel Martinez <jehielmartinez@gmail.com>
2019-07-31 04:39:29 -0600

    transfer duties to next month route added

--- routes/dutiesRoutes.js
+++ routes/dutiesRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Duty = require('../models/dutyModel')
+const moment = require('moment')
 
 //NEW DUTY
 router.post('/new-duty', async (req, res) => {
@@ -52,6 +53,25 @@ router.get('/get-duties', async (req, res) => {
         res.status(200).send(duties)
     } catch (error) {
         res.status(404).send(error)
+    }
+})
+
+//TRANSFER PREVIOUS MONTH DUTIES TO CURRENT MONTH
+router.get('/transfer-duties', async(req, res) => {
+    const currentMonth = moment().format('MM')
+    const prevMonth = moment().subtract(1, 'month').format('MM')
+    try {
+        const prevMonthDuties = await Duty.find({month: prevMonth})
+        await prevMonthDuties.forEach(duty => {
+            delete duty._id
+            delete duty.__v
+            duty.month = currentMonth
+        })
+        const duties = await Duty.create(prevMonthDuties)
+        res.status(200).send(duties)
+    } catch (error) {
+        res.status(400).send(error)
+
     }
 })
 
